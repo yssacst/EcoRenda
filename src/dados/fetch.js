e571bd433ea4e24d4a57fb7a4eed24fcf327dd52
yssacst <38430578+yssacst@users.noreply.github.com>
2021-01-21 01:21:29 -0300

    Update fetch.js
    apontando para a API

--- src/dados/fetch.js
+++ src/dados/fetch.js
@@ -1,5 +1,5 @@
 export async function getApi() {
-    const url = 'http://localhost:4000/catadores'
+    const url = 'https://ecorendaapi.herokuapp.com/catadores'
     const response = await fetch(url)
     const dados = await response.json()
 
@@ -7,7 +7,7 @@ export async function getApi() {
 }
 
 export async function getUser(params) {
-    const url = `http://localhost:4000/catadores/email=${params}`
+    const url = `https://ecorendaapi.herokuapp.com/catadores/email=${params}`
     const response = await fetch(url)
     const dados = await response.json()
 
